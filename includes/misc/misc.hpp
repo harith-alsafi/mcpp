6b2881697c2f3804133686f446b240cb1e2da790
Harith Al-Safi <el20hzaa@leeds.ac.uk>
2021-04-15 22:11:33 +0300

    started with misc functions

---
+++ includes/misc/misc.hpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <string>
+namespace misc
+{
+    template<typename S>
+    void read(std::string filename, std::vector<S>& v){
+
+    }
+
+    template<typename S>
+    void write(std::string filename, std::vector<S> v){
+
+    }
+}
--- src/plot/figure.cpp
+++ src/plot/figure.cpp
@@ -1,7 +1,2 @@
 #include "../../includes/mcpp.hpp"
 #include "../../lib/SFML-2.5.1/include/SFML/Graphics.hpp"
-
-#include <iostream>
-void check_param(int exp, int act){
-    std::cout << exp << act << std::endl;
-}
