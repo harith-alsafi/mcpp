5d4f1dabe80854c829e61c6b591167d2151d7d6d
Harith Al-Safi <harith.alsafi@gmail.com>
2021-04-18 18:28:14 +0300

    test-11

---
+++ lib/sfml_deps.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+# Install CMake GUI
+sudo apt-get install cmake-gui
+
+# SFML Dependencies
+sudo apt-get install libpthread-stubs0-dev
+sudo apt-get install libgl1-mesa-dev
+sudo apt-get install libx11-dev
+sudo apt-get install libxrandr-dev
+sudo apt-get install libfreetype6-dev
+sudo apt-get install libglew1.5-dev
+sudo apt-get install libjpeg8-dev
+sudo apt-get install libsndfile1-dev
+sudo apt-get install libopenal-dev
+
+# Joystick
+sudo apt-get install libudev-dev
